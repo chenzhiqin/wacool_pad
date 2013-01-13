ca3a1c976498e988eccec3813d65d84a23aa1f0f
邓生金 <root@localhost.(none)>
2013-01-13 14:52:57 +0800

    Initial Commit

---
+++ wacool_pad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  wacool_pad
+//
+//  Created by 邓生金 on 13-1-13.
+//  Copyright (c) 2013年 wacool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ wacool_pad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  wacool_pad
+//
+//  Created by 邓生金 on 13-1-13.
+//  Copyright (c) 2013年 wacool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
